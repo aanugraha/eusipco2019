b9bf3546e387193a86e856a24687fe9447f1ade2
Aditya Arie Nugraha <aa.nugraha@ieee.org>
2019-11-18 18:51:15 +0900

    Handled most issues of long line (re: C0301), comparison to None (re: C0121), and no-else-return (re: R1705).

--- Jointly_Diagonalizable_FullRank_Model/FastFCA.py
+++ Jointly_Diagonalizable_FullRank_Model/FastFCA.py
@@ -25,7 +25,9 @@ except:
 
 class FastFCA():
 
-    def __init__(self, NUM_source=2, xp=np, MODE_initialize_covarianceMatrix="unit"):
+    def __init__(
+            self, NUM_source=2, xp=np,
+            MODE_initialize_covarianceMatrix="unit"):
         """ initialize FastFCA
 
         Parameters:
@@ -36,9 +38,11 @@ class FastFCA():
                 how to initialize covariance matrix {unit, obs}
         """
         self.NUM_source = NUM_source
-        self.MODE_initialize_covarianceMatrix = MODE_initialize_covarianceMatrix
+        self.MODE_initialize_covarianceMatrix =\
+            MODE_initialize_covarianceMatrix
         self.xp = xp
-        self.calculateInverseMatrix = self.return_InverseMatrixCalculationMethod()
+        self.calculateInverseMatrix =\
+            self.return_InverseMatrixCalculationMethod()
         self.method_name = "FastFCA"
 
         self.NUM_freq, self.NUM_time, self.NUM_mic = None, None, None
@@ -58,23 +62,22 @@ class FastFCA():
 
 
     def convert_to_NumpyArray(self, data):
-        if self.xp == np:
-            return data
-        else:
-            return self.xp.asnumpy(data)
-
+        if self.xp != np:
+            data = self.xp.asnumpy(data)
+        return data
 
     def return_InverseMatrixCalculationMethod(self):
         if self.xp == np:
-            return np.linalg.inv
+            inv_func = np.linalg.inv
         elif FLAG_CupyInverse_Enabled:
-            return inv_gpu_batch
+            inv_func = inv_gpu_batch
         else:
-            return lambda x: self.xp.asarray(np.linalg.inv(
+            inv_func = lambda x: self.xp.asarray(np.linalg.inv(
                 self.convert_to_NumpyArray(x)))
+        return inv_func
 
-
-    def set_parameter(self, NUM_source=None, MODE_initialize_covarianceMatrix=None):
+    def set_parameter(
+            self, NUM_source=None, MODE_initialize_covarianceMatrix=None):
         """ set parameters
 
         Parameters:
@@ -84,10 +87,11 @@ class FastFCA():
             MODE_initialize_covarianceMatrix: str
                 how to initialize covariance matrix {unit, obs}
         """
-        if NUM_source != None:
+        if NUM_source is not None:
             self.NUM_source = NUM_source
-        if MODE_initialize_covarianceMatrix != None:
-            self.MODE_initialize_covarianceMatrix = MODE_initialize_covarianceMatrix
+        if MODE_initialize_covarianceMatrix is not None:
+            self.MODE_initialize_covarianceMatrix =\
+                MODE_initialize_covarianceMatrix
 
 
     def load_spectrogram(self, X_FTM):
@@ -100,7 +104,8 @@ class FastFCA():
         """
         self.NUM_freq, self.NUM_time, self.NUM_mic = X_FTM.shape
         self.X_FTM = self.xp.asarray(X_FTM, dtype=C_FP_TYPE)
-        self.XX_FTMM = self.X_FTM[:, :, :, None] @ self.X_FTM[:, :, None, :].conj()
+        self.XX_FTMM =\
+            self.X_FTM[:, :, :, None] @ self.X_FTM[:, :, None, :].conj()
 
 
     def initialize_PSD(self):
@@ -111,16 +116,26 @@ class FastFCA():
 
     def initialize_covarianceMatrix(self):
         if "unit" in self.MODE_initialize_covarianceMatrix:
-            self.diagonalizer_FMM = self.xp.tile(self.xp.eye(self.NUM_mic), [self.NUM_freq, 1, 1]).astype(C_FP_TYPE)
-            self.covarianceDiag_NFM = self.xp.ones([self.NUM_source, self.NUM_freq, self.NUM_mic], dtype=F_FP_TYPE) / self.NUM_mic
+            self.diagonalizer_FMM = self.xp.tile(
+                self.xp.eye(self.NUM_mic), [self.NUM_freq, 1, 1]).astype(
+                    C_FP_TYPE)
+            self.covarianceDiag_NFM = self.xp.ones(
+                [self.NUM_source, self.NUM_freq, self.NUM_mic],
+                dtype=F_FP_TYPE) / self.NUM_mic
         elif "obs" in self.MODE_initialize_covarianceMatrix:
-            mixture_covarianceMatrix_FMM = self.XX_FTMM.sum(axis=1) / (self.xp.trace(self.XX_FTMM, axis1=2, axis2=3).sum(axis=1))[:, None, None]
-            eig_val, eig_vec = np.linalg.eigh(self.convert_to_NumpyArray(mixture_covarianceMatrix_FMM))
-            self.diagonalizer_FMM = self.xp.asarray(eig_vec).transpose(0, 2, 1).conj()
-            self.covarianceDiag_NFM = self.xp.ones([self.NUM_source, self.NUM_freq, self.NUM_mic], dtype=F_FP_TYPE) / self.NUM_mic
+            mixture_covarianceMatrix_FMM =\
+                self.XX_FTMM.sum(axis=1) / (self.xp.trace(
+                    self.XX_FTMM, axis1=2, axis2=3).sum(axis=1))[:, None, None]
+            eig_val, eig_vec = np.linalg.eigh(
+                self.convert_to_NumpyArray(mixture_covarianceMatrix_FMM))
+            self.diagonalizer_FMM = self.xp.asarray(
+                eig_vec).transpose(0, 2, 1).conj()
+            self.covarianceDiag_NFM = self.xp.ones(
+                [self.NUM_source, self.NUM_freq, self.NUM_mic],
+                dtype=F_FP_TYPE) / self.NUM_mic
             self.covarianceDiag_NFM[0] = self.xp.asarray(eig_val)
         else:
-            print("Please specify how to initialize covariance matrix {unit, obs}")
+            print("Specify how to initialize covariance matrix {unit, obs}!")
             raise ValueError
 
         self.normalize()
@@ -128,25 +143,36 @@ class FastFCA():
 
     def reset_variable(self):
         if self.xp == np:
-            self.Qx_power_FTM = self.xp.abs((self.diagonalizer_FMM[:, None] @ self.X_FTM[:, :, :, None])[:, :, :, 0]) ** 2
+            self.Qx_power_FTM = self.xp.abs(
+                (self.diagonalizer_FMM[:, None] @ self.X_FTM[:, :, :, None])
+                [:, :, :, 0]) ** 2
         else:
-            self.Qx_power_FTM = self.xp.abs((self.diagonalizer_FMM[:, None] * self.X_FTM[:, :, None]).sum(axis=3)) ** 2
-        self.Y_FTM = (self.lambda_NFT[..., None] * self.covarianceDiag_NFM[:, :, None]).sum(axis=0)
+            self.Qx_power_FTM = self.xp.abs(
+                (self.diagonalizer_FMM[:, None] * self.X_FTM[:, :, None])
+                .sum(axis=3)) ** 2
+        self.Y_FTM = (
+            self.lambda_NFT[..., None] *
+            self.covarianceDiag_NFM[:, :, None]).sum(axis=0)
 
 
     def make_fileName_suffix(self):
-        self.fileName_suffix = "S={}-it={}-init={}".format(self.NUM_source, self.NUM_iteration, self.MODE_initialize_covarianceMatrix)
+        self.fileName_suffix = "S={}-it={}-init={}".format(
+            self.NUM_source, self.NUM_iteration,
+            self.MODE_initialize_covarianceMatrix)
 
         if hasattr(self, "file_id"):
             self.fileName_suffix += "-ID={}".format(self.file_id)
         else:
-            print("====================\n\nWarning: Please set self.file_id\n\n====================")
+            print("========\n\nWarning: Please set self.file_id\n\n========")
 
         print("parameter:", self.fileName_suffix)
         return self.fileName_suffix
 
 
-    def solve(self, NUM_iteration=100, save_likelihood=False, save_parameter=False, save_wav=False, save_path="./", interval_save_parameter=30, mic_index=MIC_INDEX):
+    def solve(
+            self, NUM_iteration=100, mic_index=MIC_INDEX,
+            save_likelihood=False, save_parameter=False, save_wav=False,
+            save_path="./", interval_save_parameter=30):
         """
         Parameters:
             save_likelihood: boolean
@@ -170,25 +196,40 @@ class FastFCA():
         for it in tqdm(range(self.NUM_iteration)):
             self.update()
 
-            if save_parameter and ((it+1) % interval_save_parameter == 0) and ((it+1) != self.NUM_iteration):
-                self.save_parameter(save_path+"{}-parameter-{}-{}.pic".format(self.method_name, self.fileName_suffix, it + 1))
+            if save_parameter and ((it+1) % interval_save_parameter == 0) and\
+                    ((it+1) != self.NUM_iteration):
+                self.save_parameter(
+                    save_path+"{}-parameter-{}-{}.pic".format(
+                        self.method_name, self.fileName_suffix, it + 1))
 
-            if save_wav and ((it+1) % interval_save_parameter == 0) and ((it+1) != self.NUM_iteration):
+            if save_wav and ((it+1) % interval_save_parameter == 0) and\
+                    ((it+1) != self.NUM_iteration):
                 self.separate_FastWienerFilter(mic_index=MIC_INDEX)
-                self.save_separated_signal(save_path+"{}-sep-Wiener-{}-{}.wav".format(self.method_name, self.fileName_suffix, it + 1))
+                self.save_separated_signal(
+                    save_path+"{}-sep-Wiener-{}-{}.wav".format(
+                        self.method_name, self.fileName_suffix, it + 1))
 
-            if save_likelihood and ((it+1) % interval_save_parameter == 0) and ((it+1) != self.NUM_iteration):
+            if save_likelihood and ((it+1) % interval_save_parameter == 0) and\
+                    ((it+1) != self.NUM_iteration):
                 log_likelihood_array.append(self.calculate_log_likelihood())
 
         if save_parameter:
-            self.save_parameter(save_path+"{}-parameter-{}.pic".format(self.method_name, self.fileName_suffix))
+            self.save_parameter(
+                save_path+"{}-parameter-{}.pic".format(
+                    self.method_name, self.fileName_suffix))
 
         if save_likelihood:
             log_likelihood_array.append(self.calculate_log_likelihood())
-            pic.dump(log_likelihood_array, open(save_path + "{}-likelihood-interval={}-{}.pic".format(self.method_name, interval_save_parameter, self.fileName_suffix), "wb"))
+            pic.dump(
+                log_likelihood_array, open(
+                    save_path + "{}-likelihood-interval={}-{}.pic".format(
+                        self.method_name, interval_save_parameter,
+                        self.fileName_suffix), "wb"))
 
         self.separate_FastWienerFilter(mic_index=mic_index)
-        self.save_separated_signal(save_path+"{}-sep-Wiener-{}.wav".format(self.method_name, self.fileName_suffix))
+        self.save_separated_signal(
+            save_path+"{}-sep-Wiener-{}.wav".format(
+                self.method_name, self.fileName_suffix))
 
 
     def update(self):
@@ -200,10 +241,13 @@ class FastFCA():
 
     def update_Diagonalizer(self):
         # Eq. (18): V_fm = 1/T * sum_t X_ft / \tilde{y}_ftm
-        V_FMMM = (self.XX_FTMM[:, :, None] / self.Y_FTM[:, :, :, None, None]).mean(axis=1)
+        V_FMMM = (
+            self.XX_FTMM[:, :, None] / self.Y_FTM[:, :, :, None, None]).mean(
+                axis=1)
         for m in range(self.NUM_mic):
             # Eq. (19): q_fm <- (Q_f V_fm)^{-1} e_m
-            tmp_FM = self.calculateInverseMatrix(self.diagonalizer_FMM @ V_FMMM[:, m])[:, :, m]
+            tmp_FM = self.calculateInverseMatrix(
+                self.diagonalizer_FMM @ V_FMMM[:, m])[:, :, m]
             # Eq. (20): q_fm <- q_fm / sqrt(q_fm^H V_fm q_fm)
             tmp_FM /= self.xp.sqrt(self.xp.sum(
                 tmp_FM.conj()[:, :, None] * V_FMMM[:, m] * tmp_FM[:, None, :],
@@ -212,18 +256,26 @@ class FastFCA():
 
 
     def update_CovarianceDiagElement(self):
-        a_1 = (self.lambda_NFT[..., None] * (self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=2) # N F T M
+        a_1 = (self.lambda_NFT[..., None] * (
+            self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=2) # N F T M
         b_1 = (self.lambda_NFT[..., None] / self.Y_FTM[None]).sum(axis=2)
-        self.covarianceDiag_NFM = self.covarianceDiag_NFM * self.xp.sqrt(a_1 / b_1)
+        self.covarianceDiag_NFM = self.covarianceDiag_NFM * self.xp.sqrt(
+            a_1 / b_1)
         self.covarianceDiag_NFM += EPS
-        self.Y_FTM = (self.lambda_NFT[..., None] * self.covarianceDiag_NFM[:, :, None]).sum(axis=0)
+        self.Y_FTM = (
+            self.lambda_NFT[..., None] *
+            self.covarianceDiag_NFM[:, :, None]).sum(axis=0)
 
 
     def update_lambda(self):
-        a = (self.covarianceDiag_NFM[:, :, None] * (self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=3) # N F T
-        b = (self.covarianceDiag_NFM[:, :, None] / self.Y_FTM[None]).sum(axis=3)
+        a = (self.covarianceDiag_NFM[:, :, None] * (
+            self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=3) # N F T
+        b = (
+            self.covarianceDiag_NFM[:, :, None] / self.Y_FTM[None]).sum(axis=3)
         self.lambda_NFT = self.lambda_NFT * self.xp.sqrt(a / b) + EPS
-        self.Y_FTM = (self.lambda_NFT[..., None] * self.covarianceDiag_NFM[:, :, None]).sum(axis=0)
+        self.Y_FTM = (
+            self.lambda_NFT[..., None] *
+            self.covarianceDiag_NFM[:, :, None]).sum(axis=0)
 
 
     def normalize(self):
@@ -242,22 +294,29 @@ class FastFCA():
 
 
     def calculate_log_likelihood(self):
-        return (-(self.Qx_power_FTM / self.Y_FTM).sum() + self.NUM_time * np.log(np.linalg.det(self.convert_to_NumpyArray(self.diagonalizer_FMM @ self.diagonalizer_FMM.conj().transpose(0, 2, 1)))).sum() - self.xp.log(self.Y_FTM).sum()).real - self.NUM_mic * self.NUM_freq * self.NUM_time * np.log(np.pi)
+        return (
+            -(self.Qx_power_FTM / self.Y_FTM).sum() + self.NUM_time * np.log(np.linalg.det(self.convert_to_NumpyArray(self.diagonalizer_FMM @ self.diagonalizer_FMM.conj().transpose(0, 2, 1)))).sum() - self.xp.log(self.Y_FTM).sum()).real - self.NUM_mic * self.NUM_freq * self.NUM_time * np.log(np.pi)
 
 
     def calculate_covarianceMatrix(self):
-        covarianceMatrix_NFMM = self.xp.zeros([self.NUM_source, self.NUM_freq, self.NUM_mic, self.NUM_mic], dtype=C_FP_TYPE)
-        diagonalizer_inv_FMM = self.calculateInverseMatrix(self.diagonalizer_FMM)
+        covarianceMatrix_NFMM = self.xp.zeros(
+            [self.NUM_source, self.NUM_freq, self.NUM_mic, self.NUM_mic],
+            dtype=C_FP_TYPE)
+        diagonalizer_inv_FMM = self.calculateInverseMatrix(
+            self.diagonalizer_FMM)
         for n in range(self.NUM_source):
             for f in range(self.NUM_freq):
-                covarianceMatrix_NFMM[n, f] = diagonalizer_inv_FMM[f] @ np.diag(self.covarianceDiag_NFM[n, f]) @ diagonalizer_inv_FMM[f].conj().T
+                covarianceMatrix_NFMM[n, f] =\
+                    diagonalizer_inv_FMM[f] @ np.diag(
+                        self.covarianceDiag_NFM[n, f]) @\
+                    diagonalizer_inv_FMM[f].conj().T
         return covarianceMatrix_NFMM
 
 
     def separate_FastWienerFilter(self, source_index=None, mic_index=MIC_INDEX):
         Qx_FTM = xp.sum(
             self.diagonalizer_FMM[:, None] * self.X_FTM[:, :, None], axis=3)
-        if source_index != None:
+        if source_index is not None:
             diagonalizer_inv_FMM = self.calculateInverseMatrix(self.diagonalizer_FMM)
             self.separated_spec = self.convert_to_NumpyArray((diagonalizer_inv_FMM[:, None] @ (Qx_FTM * ((self.lambda_NFT[source_index, :, :, None] * self.covarianceDiag_NFM[source_index, :, None]) / (self.lambda_NFT[..., None]* self.covarianceDiag_NFM[:, :, None]).sum(axis=0)))[..., None])[:, :, mic_index, 0])
         else:
