993841ae8cc32dddd544c51c893674ccbf2c0897
Aditya Arie Nugraha <aa.nugraha@ieee.org>
2019-11-19 12:27:40 +0900

    Used a YAML-based config file and did minimal refactoring.

--- Jointly_Diagonalizable_FullRank_Model/FastFCA.py
+++ Jointly_Diagonalizable_FullRank_Model/FastFCA.py
@@ -1,24 +1,24 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+import os
 import sys
 import pickle as pic
 
 import argparse
 import numpy as np
 import soundfile as sf
+import yaml
 
 from scipy.signal import stft, istft, get_window
 from tqdm import tqdm
-
-from configure_FastModel import *
 
 
 sys.path.append("../CupyLibrary")
 try:
     from cupy_matrix_inverse import inv_gpu_batch
     FLAG_CupyInverse_Enabled = True
-except:
+except ImportError:
     print("---Warning--- You cannot use cupy inverse calculation")
     FLAG_CupyInverse_Enabled = False
 
@@ -60,8 +60,7 @@ class FastFCA():
         self.NUM_iteration = None
         self.separated_spec = None
 
-
-    def convert_to_NumpyArray(self, data):
+    def asnumpy(self, data):
         if self.xp != np:
             data = self.xp.asnumpy(data)
         return data
@@ -72,8 +71,9 @@ class FastFCA():
         elif FLAG_CupyInverse_Enabled:
             inv_func = inv_gpu_batch
         else:
-            inv_func = lambda x: self.xp.asarray(np.linalg.inv(
-                self.convert_to_NumpyArray(x)))
+            def inv_helper(x):
+                return self.xp.asarray(np.linalg.inv(self.asnumpy(x)))
+            inv_func = inv_helper
         return inv_func
 
     def set_parameter(
@@ -93,7 +93,6 @@ class FastFCA():
             self.MODE_initialize_covarianceMatrix =\
                 MODE_initialize_covarianceMatrix
 
-
     def load_spectrogram(self, X_FTM):
         """ load complex spectrogram
 
@@ -106,13 +105,14 @@ class FastFCA():
         self.X_FTM = self.xp.asarray(X_FTM, dtype=C_FP_TYPE)
         self.XX_FTMM =\
             self.X_FTM[:, :, :, None] @ self.X_FTM[:, :, None, :].conj()
-
+        XX_FTMM = self.xp.einsum(
+            '...i,...j->...ij', self.X_FTM, self.X_FTM.conj())
+        assert self.xp.allclose(self.XX_FTMM, XX_FTMM)
 
     def initialize_PSD(self):
         self.lambda_NFT = self.xp.random.random(
             [self.NUM_source, self.NUM_freq, self.NUM_time]).astype(F_FP_TYPE)
         self.lambda_NFT[0] = self.xp.abs(self.X_FTM.mean(axis=2)) ** 2
-
 
     def initialize_covarianceMatrix(self):
         if "unit" in self.MODE_initialize_covarianceMatrix:
@@ -127,7 +127,7 @@ class FastFCA():
                 self.XX_FTMM.sum(axis=1) / (self.xp.trace(
                     self.XX_FTMM, axis1=2, axis2=3).sum(axis=1))[:, None, None]
             eig_val, eig_vec = np.linalg.eigh(
-                self.convert_to_NumpyArray(mixture_covarianceMatrix_FMM))
+                self.asnumpy(mixture_covarianceMatrix_FMM))
             self.diagonalizer_FMM = self.xp.asarray(
                 eig_vec).transpose(0, 2, 1).conj()
             self.covarianceDiag_NFM = self.xp.ones(
@@ -139,7 +139,6 @@ class FastFCA():
             raise ValueError
 
         self.normalize()
-
 
     def reset_variable(self):
         if self.xp == np:
@@ -154,7 +153,6 @@ class FastFCA():
             self.lambda_NFT[..., None] *
             self.covarianceDiag_NFM[:, :, None]).sum(axis=0)
 
-
     def make_fileName_suffix(self):
         self.fileName_suffix = "S={}-it={}-init={}".format(
             self.NUM_source, self.NUM_iteration,
@@ -168,9 +166,8 @@ class FastFCA():
         print("parameter:", self.fileName_suffix)
         return self.fileName_suffix
 
-
     def solve(
-            self, NUM_iteration=100, mic_index=MIC_INDEX,
+            self, NUM_iteration=100, sep_mic_idx=None,
             save_likelihood=False, save_parameter=False, save_wav=False,
             save_path="./", interval_save_parameter=30):
         """
@@ -196,26 +193,25 @@ class FastFCA():
         for it in tqdm(range(self.NUM_iteration)):
             self.update()
 
-            if save_parameter and ((it+1) % interval_save_parameter == 0) and\
-                    ((it+1) != self.NUM_iteration):
+            flag_save_it = (it + 1) % interval_save_parameter == 0 and\
+                ((it + 1) != self.NUM_iteration)
+            if save_parameter and flag_save_it:
                 self.save_parameter(
-                    save_path+"{}-parameter-{}-{}.pic".format(
+                    save_path + "{}-parameter-{}-{}.pic".format(
                         self.method_name, self.fileName_suffix, it + 1))
 
-            if save_wav and ((it+1) % interval_save_parameter == 0) and\
-                    ((it+1) != self.NUM_iteration):
-                self.separate_FastWienerFilter(mic_index=MIC_INDEX)
+            if save_wav and flag_save_it:
+                self.separate_FastWienerFilter(mic_idx=sep_mic_idx)
                 self.save_separated_signal(
-                    save_path+"{}-sep-Wiener-{}-{}.wav".format(
+                    save_path + "{}-sep-Wiener-{}-{}.wav".format(
                         self.method_name, self.fileName_suffix, it + 1))
 
-            if save_likelihood and ((it+1) % interval_save_parameter == 0) and\
-                    ((it+1) != self.NUM_iteration):
+            if save_likelihood and flag_save_it:
                 log_likelihood_array.append(self.calculate_log_likelihood())
 
         if save_parameter:
             self.save_parameter(
-                save_path+"{}-parameter-{}.pic".format(
+                save_path + "{}-parameter-{}.pic".format(
                     self.method_name, self.fileName_suffix))
 
         if save_likelihood:
@@ -226,18 +222,16 @@ class FastFCA():
                         self.method_name, interval_save_parameter,
                         self.fileName_suffix), "wb"))
 
-        self.separate_FastWienerFilter(mic_index=mic_index)
+        self.separate_FastWienerFilter(mic_idx=sep_mic_idx)
         self.save_separated_signal(
-            save_path+"{}-sep-Wiener-{}.wav".format(
+            save_path + "{}-sep-Wiener-{}.wav".format(
                 self.method_name, self.fileName_suffix))
-
 
     def update(self):
         self.update_lambda()
         self.update_CovarianceDiagElement()
         self.update_Diagonalizer()
         self.normalize()
-
 
     def update_Diagonalizer(self):
         # Eq. (18): V_fm = 1/T * sum_t X_ft / \tilde{y}_ftm
@@ -254,10 +248,9 @@ class FastFCA():
                 axis=(-2, -1)))[:, None]
             self.diagonalizer_FMM[:, m] = tmp_FM.conj()
 
-
     def update_CovarianceDiagElement(self):
         a_1 = (self.lambda_NFT[..., None] * (
-            self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=2) # N F T M
+            self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=2)  # NFTM
         b_1 = (self.lambda_NFT[..., None] / self.Y_FTM[None]).sum(axis=2)
         self.covarianceDiag_NFM = self.covarianceDiag_NFM * self.xp.sqrt(
             a_1 / b_1)
@@ -266,17 +259,15 @@ class FastFCA():
             self.lambda_NFT[..., None] *
             self.covarianceDiag_NFM[:, :, None]).sum(axis=0)
 
-
     def update_lambda(self):
         a = (self.covarianceDiag_NFM[:, :, None] * (
-            self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=3) # N F T
+            self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=3)  # NFT
         b = (
             self.covarianceDiag_NFM[:, :, None] / self.Y_FTM[None]).sum(axis=3)
         self.lambda_NFT = self.lambda_NFT * self.xp.sqrt(a / b) + EPS
         self.Y_FTM = (
             self.lambda_NFT[..., None] *
             self.covarianceDiag_NFM[:, :, None]).sum(axis=0)
-
 
     def normalize(self):
         phi_F = self.xp.sum(
@@ -292,11 +283,15 @@ class FastFCA():
 
         self.reset_variable()
 
-
     def calculate_log_likelihood(self):
-        return (
-            -(self.Qx_power_FTM / self.Y_FTM).sum() + self.NUM_time * np.log(np.linalg.det(self.convert_to_NumpyArray(self.diagonalizer_FMM @ self.diagonalizer_FMM.conj().transpose(0, 2, 1)))).sum() - self.xp.log(self.Y_FTM).sum()).real - self.NUM_mic * self.NUM_freq * self.NUM_time * np.log(np.pi)
-
+        return ((-1 * (
+            self.Qx_power_FTM / self.Y_FTM).sum() +
+            self.NUM_time * np.log(np.linalg.det(
+                self.asnumpy(
+                    self.diagonalizer_FMM @
+                    self.diagonalizer_FMM.conj().transpose(0, 2, 1)))).sum() -
+            self.xp.log(self.Y_FTM).sum()).real -
+            self.NUM_mic * self.NUM_freq * self.NUM_time * np.log(np.pi))
 
     def calculate_covarianceMatrix(self):
         covarianceMatrix_NFMM = self.xp.zeros(
@@ -312,43 +307,58 @@ class FastFCA():
                     diagonalizer_inv_FMM[f].conj().T
         return covarianceMatrix_NFMM
 
-
-    def separate_FastWienerFilter(self, source_index=None, mic_index=MIC_INDEX):
+    def separate_FastWienerFilter(self, src_idx=None, mic_idx=None):
         Qx_FTM = xp.sum(
             self.diagonalizer_FMM[:, None] * self.X_FTM[:, :, None], axis=3)
-        if source_index is not None:
-            diagonalizer_inv_FMM = self.calculateInverseMatrix(self.diagonalizer_FMM)
-            self.separated_spec = self.convert_to_NumpyArray((diagonalizer_inv_FMM[:, None] @ (Qx_FTM * ((self.lambda_NFT[source_index, :, :, None] * self.covarianceDiag_NFM[source_index, :, None]) / (self.lambda_NFT[..., None]* self.covarianceDiag_NFM[:, :, None]).sum(axis=0)))[..., None])[:, :, mic_index, 0])
+        diagonalizer_inv_FMM = self.calculateInverseMatrix(
+            self.diagonalizer_FMM)
+        if mic_idx is None:
+            raise NotImplementedError
+        if src_idx is not None:
+            self.separated_spec = self.asnumpy((
+                diagonalizer_inv_FMM[:, None] @ (
+                    Qx_FTM * (
+                        (self.lambda_NFT[src_idx, :, :, None] *
+                            self.covarianceDiag_NFM[src_idx, :, None]) /
+                        (self.lambda_NFT[..., None] *
+                            self.covarianceDiag_NFM[:, :, None]).sum(axis=0)))
+                [..., None])[:, :, mic_idx, 0])
         else:
             for n in range(self.NUM_source):
-                diagonalizer_inv_FMM = self.calculateInverseMatrix(self.diagonalizer_FMM)
-                tmp = self.convert_to_NumpyArray((diagonalizer_inv_FMM[:, None] @ (Qx_FTM * ((self.lambda_NFT[n, :, :, None] * self.covarianceDiag_NFM[n, :, None]) / (self.lambda_NFT[..., None]* self.covarianceDiag_NFM[:, :, None]).sum(axis=0)))[..., None])[:, :, mic_index, 0])
+                tmp = self.asnumpy((
+                    diagonalizer_inv_FMM[:, None] @ (
+                        Qx_FTM * (
+                            (self.lambda_NFT[n, :, :, None] *
+                                self.covarianceDiag_NFM[n, :, None]) /
+                            (self.lambda_NFT[..., None] *
+                                self.covarianceDiag_NFM[:, :, None])
+                            .sum(axis=0)))
+                    [..., None])[:, :, mic_idx, 0])
                 if n == 0:
-                    self.separated_spec = np.zeros([self.NUM_source, tmp.shape[0], tmp.shape[1]], dtype=np.complex)
+                    self.separated_spec = np.zeros(
+                        [self.NUM_source, tmp.shape[0], tmp.shape[1]],
+                        dtype=np.complex)
                 self.separated_spec[n] = tmp
 
-
     def save_separated_signal(self, save_fileName="sample.wav"):
-        sep_src_stft_MFN = self.convert_to_NumpyArray(self.separated_spec)
+        sep_src_stft_MFN = self.asnumpy(self.separated_spec)
         # infer STFT length and scale (= get_window('hann', args.n_fft).sum())
         stft_winlen = (sep_src_stft_MFN.shape[1] - 1) * 2
         stft_scale = sep_src_stft_MFN.shape[1] - 1
         sep_src_stft_MFN /= stft_scale
         _, sep_src_sig_MT = istft(
             sep_src_stft_MFN, window='hann', nperseg=stft_winlen,
-            noverlap=3*stft_winlen//4, nfft=stft_winlen)
-        sep_src_sig_TM = sep_src_sig_MT.T #[:mix_sig_TM.shape[0]]
+            noverlap=int(3 * stft_winlen // 4), nfft=stft_winlen)
+        sep_src_sig_TM = sep_src_sig_MT.T  # [:mix_sig_TM.shape[0]]
         sf.write(save_fileName, sep_src_sig_TM, 16000)
-
 
     def save_parameter(self, fileName):
         param_list = [
             self.lambda_NFT, self.covarianceDiag_NFM, self.diagonalizer_FMM]
         if self.xp != np:
             param_list = [
-                self.convert_to_NumpyArray(param) for param in param_list]
+                self.asnumpy(param) for param in param_list]
         pic.dump(param_list, open(fileName, "wb"))
-
 
     def load_parameter(self, fileName):
         param_list = pic.load(open(fileName, "rb"))
@@ -364,6 +374,9 @@ if __name__ == "__main__":
     parser.add_argument(
         'input_fileName', help='filename of the multichannel observed signals',
         type=str)
+    parser.add_argument(
+        '--config', help='config file',
+        type=str, default="config.yaml")
     parser.add_argument(
         '--file_id', help='file id',
         type=str, default="None")
@@ -389,6 +402,17 @@ if __name__ == "__main__":
         '--single_fp', action='store_true', dest="single_fp", default=False)
     args = parser.parse_args()
 
+    if os.path.isfile(args.config):
+        with open(args.config, 'r') as f:
+            CONF = yaml.load(f, Loader=yaml.FullLoader)
+        print(yaml.dump(CONF))
+        EPS = float(CONF['eps'])
+        SEP_MIC_IDX = CONF['sep_mic_idx']
+    else:
+        print("Config file is not found! Use the default values.")
+        EPS = 1e-07  # re: np.finfo('float32').eps = 1.19e-07
+        SEP_MIC_IDX = 4
+
     if args.gpu < 0:
         xp = np
     else:
@@ -406,7 +430,7 @@ if __name__ == "__main__":
     sig, fs = sf.read(args.input_fileName, always_2d=True)
     spec_FNM = np.transpose(
         stft(sig.T, window='hann',
-             nperseg=args.n_fft, noverlap=3*args.n_fft//4)[-1],
+             nperseg=args.n_fft, noverlap=int(3 * args.n_fft // 4))[-1],
         (1, 2, 0))
     stft_scale = get_window('hann', args.n_fft).sum()
     spec_FNM *= stft_scale
@@ -417,6 +441,6 @@ if __name__ == "__main__":
     separater.load_spectrogram(spec_FNM)
     separater.file_id = args.file_id
     separater.solve(
-        NUM_iteration=args.NUM_iteration,
+        NUM_iteration=args.NUM_iteration, sep_mic_idx=SEP_MIC_IDX,
         save_likelihood=False, save_parameter=False, save_wav=False,
         save_path="./", interval_save_parameter=25)
